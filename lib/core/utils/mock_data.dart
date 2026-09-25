b7618c0edfb61ad382fb70d7f3d1c1b94d70cff7
joaopohlmannneo7 <joaopohlmann7@gmail.com>
2026-09-24 23:41:28 -0300

    feat(paciente): criar modelo de dados e mock de motorista

---
+++ lib/core/utils/mock_data.dart
@@ -0,0 +1,9 @@
+final Map<String, dynamic> motoristaJson={
+  "nome" : "Carlos Roberto",
+  "veiculo": "Spin Prata",
+  "placa": "ABC-1D23",
+  "tempoEstimado": "12 minutos",
+  "status": "EM TRÂNSITO",
+  "titulo": "Seu Transporte da Tarde",
+
+};
