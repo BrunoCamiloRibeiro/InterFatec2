fa005d55b86ca9bdcf56bc782f08923e45872c70
brunocr <brunocamiloribeirop@gmail.com>
2026-09-25 10:16:07 -0300

    chore: remover arquivos e pastas antigos da arquitetura anterior

--- lib/core/utils/mock_data.dart
+++
@@ -1,9 +0,0 @@
-final Map<String, dynamic> motoristaJson={
-  "nome" : "Carlos Roberto",
-  "veiculo": "Spin Prata",
-  "placa": "ABC-1D23",
-  "tempoEstimado": "12 minutos",
-  "status": "EM TRÂNSITO",
-  "titulo": "Seu Transporte da Tarde",
-
-};
