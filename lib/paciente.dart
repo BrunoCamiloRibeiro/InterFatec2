22b865744623045e0de8a27e0a81bb0d1751cd2c
brunocr <brunocamiloribeirop@gmail.com>
2026-09-25 11:04:54 -0300

    refactor(style): erradicar todos os const e trocar scaffold por material
    o professor aparentemente tem fobia de const entao mandei tudo pro espaco, e scaffold foi banido pq falaram pra usar material em tudo. se o linter reclamar a culpa e dele.

--- lib/paciente.dart
+++ lib/paciente.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
+// tela do paciente sem Scaffold e usando Material
 class Paciente extends StatelessWidget {
-  const Paciente({super.key});
+  Paciente({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,130 +11,148 @@ class Paciente extends StatelessWidget {
     final String motoristaNome = "Carlos Roberto";
     final String veiculoInfo = "Spin Prata • Placa: ABC-1D23";
 
-    return Scaffold(
-      appBar: AppBar(title: const Text('Painel')),
-      body: Padding(
-        padding: const EdgeInsets.all(9.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Text(
-              'Painel Paciente',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const Text(
-              'Status do seu transporte de saúde',
-              style: TextStyle(color: Colors.grey),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.05),
-                    blurRadius: 5,
+    return Material(
+      color: Colors.white,
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // botão voltar já que não tem AppBar do Scaffold
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.black),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Text(
+                    'Painel',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                 ],
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    titulo,
-                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  const SizedBox(height: 4),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Text(
-                      status,
-                      style: const TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Row(
+              SizedBox(height: 10),
+              Text(
+                'Painel Paciente',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Status do seu transporte de saúde',
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(height: 20),
+              Material(
+                color: Colors.white,
+                elevation: 2,
+                borderRadius: BorderRadius.circular(8),
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const SizedBox(width: 12),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Motorista: $motoristaNome',
-                            style: const TextStyle(fontWeight: FontWeight.bold),
+                      Text(
+                        titulo,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Material(
+                        color: Colors.grey.shade200,
+                        borderRadius: BorderRadius.circular(12),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 4,
                           ),
-                          Text(
-                            veiculoInfo,
+                          child: Text(
+                            status,
                             style: TextStyle(
-                              color: Colors.grey.shade600,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
                               fontSize: 12,
                             ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        children: [
+                          SizedBox(width: 12),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Motorista: $motoristaNome',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                veiculoInfo,
+                                style: TextStyle(
+                                  color: Colors.grey.shade600,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
-            ),
-            const Spacer(),
-            Center(
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    child: const Text('Já consegui Carona'),
-                    onPressed: () {
-                      _mostrarDialog(
-                        context: context,
-                        titulo: "Confirmação de Carona",
-                        mensagem: "Você confirma que já conseguiu carona?",
-                        onConfirmar: () {
-                          debugPrint("O motorista será avisado");
-                        },
-                        onCancelar: () {
-                          debugPrint("Ação cancelada pelo usuário");
-                        },
-                      );
-                    },
-                  ),
-                  const SizedBox(width: 12),
-                  ElevatedButton(
-                    child: const Text("Estou pronto"),
-                    onPressed: () {
-                      _mostrarDialog(
-                        context: context,
-                        titulo: "Confirmação de partida",
-                        mensagem:
-                            "Tem certeza de que está pronto? O motorista será notificado da sua prontidão.",
-                        onConfirmar: () {
-                          const nomePaciente = "pacienteX";
-                          debugPrint(
-                            "Confirmação enviada! O motorista foi alertado de que $nomePaciente está pronto.",
-                          );
-                        },
-                        onCancelar: () {
-                          debugPrint("O paciente cancelou a ação.");
-                        },
-                      );
-                    },
-                  ),
-                ],
+              Spacer(),
+              Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      child: Text('Já consegui Carona'),
+                      onPressed: () {
+                        _mostrarDialog(
+                          context: context,
+                          titulo: "Confirmação de Carona",
+                          mensagem: "Você confirma que já conseguiu carona?",
+                          onConfirmar: () {
+                            debugPrint("O motorista será avisado");
+                          },
+                          onCancelar: () {
+                            debugPrint("Ação cancelada pelo usuário");
+                          },
+                        );
+                      },
+                    ),
+                    SizedBox(width: 12),
+                    ElevatedButton(
+                      child: Text("Estou pronto"),
+                      onPressed: () {
+                        _mostrarDialog(
+                          context: context,
+                          titulo: "Confirmação de partida",
+                          mensagem: "Tem certeza de que está pronto? O motorista será notificado da sua prontidão.",
+                          onConfirmar: () {
+                            final nomePaciente = "pacienteX";
+                            debugPrint(
+                              "Confirmação enviada! O motorista foi alertado de que $nomePaciente está pronto.",
+                            );
+                          },
+                          onCancelar: () {
+                            debugPrint("O paciente cancelou a ação.");
+                          },
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-          ],
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
@@ -154,14 +173,14 @@ class Paciente extends StatelessWidget {
           content: Text(mensagem),
           actions: <Widget>[
             TextButton(
-              child: const Text('Sim'),
+              child: Text('Sim'),
               onPressed: () {
                 Navigator.of(context).pop();
                 onConfirmar();
               },
             ),
             TextButton(
-              child: const Text('Não'),
+              child: Text('Não'),
               onPressed: () {
                 Navigator.of(context).pop();
                 if (onCancelar != null) onCancelar();
